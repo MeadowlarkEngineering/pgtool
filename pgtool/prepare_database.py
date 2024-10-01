5ef54d6c27ff3228419efa8076f10a19b858e2b0
Joshua Shapiro <joshua.shapiro@gmail.com>
2024-10-01 10:48:10 -0700

    Added rollback and return values

--- pgtool/prepare_database.py
+++ pgtool/prepare_database.py
@@ -21,6 +21,7 @@ def main():
     parser.add_argument("db_url", nargs='?', help="Database URL (If not provided will use DB_URL environment variable)")
     parser.add_argument("--db-name-suffix", help="Adds a suffix to the database name separated by an underscore")
     parser.add_argument("--migrations-path", help="Path to the migrations directory", default="migrations")
+    parser.add_argument("--rollback", type=int, help="Rollback N migrations. Use 0 to list available", default=None)
 
     args = parser.parse_args()
 
@@ -39,9 +40,9 @@ def main():
     user = url.username
     password = url.password
 
-    prepare(user, password, server, args.db_name_suffix, args.migrations_path)
+    prepare(user, password, server, args.db_name_suffix, args.migrations_path, args.rollback)
 
-def prepare(user, password, host, db_name_suffix = None, migration_path = 'migrations'):
+def prepare(user, password, host, db_name_suffix = None, migration_path = 'migrations', rollback=None):
     """
     Prepare all databases in the migrations folder
     """
@@ -64,14 +65,33 @@ def prepare(user, password, host, db_name_suffix = None, migration_path = 'migra
                 cur.execute(f"CREATE DATABASE {db_name}")
             conn.close()
 
-        print(f"Applying migrations in {database_path}")
-        migrations = read_migrations(str(database_path))
-    
         db_url = f"postgres://{user}:{password}@{host}/{db_name}"
         backend = get_backend(db_url)
 
+
+        migrations = read_migrations(str(database_path))
+
         with backend.lock():
-            backend.apply_migrations(backend.to_apply(migrations))
+            if rollback is not None:
+                available = backend.to_rollback(migrations)
+                rollback_n = rollback
+                if len(available) < rollback_n:
+                    return{"error": "Unable to rollback that far. Use --rollback 0 to see available rollback"}
+
+                if rollback_n == 0:
+                    print(f"Available migrations to rollback in {database_path}")
+                    for i, m in enumerate(available):
+                        print(f"{i+1}: {m.path}")
+                    return {"rollback_migrations": [{i+1: m.path} for i, m in enumerate(available)]}
+                else:
+                    backend.rollback_migrations(available[:rollback_n])
+                    return 
+            else:
+                print(f"Applying migrations in {database_path}")
+    
+                to_apply = backend.to_apply(migrations) 
+                backend.apply_migrations(backend.to_apply(migrations))
+                return {"applied_migrations": [{i+1: m.path} for i, m in enumerate(to_apply)]}
 
 if __name__ == "__main__":
     main()
